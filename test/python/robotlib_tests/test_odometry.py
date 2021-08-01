ccc78cf7ae3de669891a24d9a194f87d024053bd
Austin Bowen <austin.bowen.314@gmail.com>
2021-07-31 22:32:03 -0400

    Put tests in their own package robotlib_tests. Rename tests for clarity.

--- test/python/robotlib_tests/test_odometry.py
+++ test/python/robotlib_tests/test_odometry.py
@@ -1,19 +1,19 @@
 import unittest
 
-from robotlib.odometry import DifferentialOdometry
+from robotlib import odometry
 from math import pi
 
 
-class TestDifferentialOdometryUpdateFromVelocity(unittest.TestCase):
+class DifferentialOdometryUpdateFromVelocityTest(unittest.TestCase):
     def assert_odom_equal(self, x: float, y: float, heading: float):
         self.assertAlmostEqual(self.odom.x, x)
         self.assertAlmostEqual(self.odom.y, y)
         self.assertAlmostEqual(self.odom.heading, heading)
 
     def setUp(self):
-        self.odom = DifferentialOdometry(1.0)
+        self.odom = odometry.DifferentialOdometry(1.0)
 
-    def test_update_result(self):
+    def test_update__result(self):
         expected_keys = {'x', 'd_x', 'y', 'd_y', 'heading', 'd_heading', 'd_t'}
 
         result = self.odom.update_from_velocity(0, 0, 0)
@@ -21,48 +21,48 @@ class TestDifferentialOdometryUpdateFromVelocity(unittest.TestCase):
 
         self.assertSetEqual(actual_keys, expected_keys)
 
-    def test_update_stopped(self):
+    def test_update__stopped(self):
         self.odom.update_from_velocity(0, 0, 1)
         self.assert_odom_equal(0, 0, 0)
 
-    def test_update_forward(self):
+    def test_update__forward(self):
         self.odom.update_from_velocity(1, 1, 1)
         self.assert_odom_equal(1, 0, 0)
 
-    def test_update_backward(self):
+    def test_update__backward(self):
         self.odom.update_from_velocity(-1, -1, 1)
         self.assert_odom_equal(-1, 0, 0)
 
-    def test_update_left(self):
+    def test_update__left(self):
         self.odom.update_from_velocity(-1, 1, 1)
         self.assert_odom_equal(0, 0, 2)
 
-    def test_update_right(self):
+    def test_update__right(self):
         self.odom.update_from_velocity(1, -1, 1)
         self.assert_odom_equal(0, 0, -2)
 
-    def test_update_forward_left(self):
+    def test_update__forward_left(self):
         self.odom.update_from_velocity(0, pi / 2, 1)
         self.assert_odom_equal(0.5, 0.5, pi / 2)
 
-    def test_update_forward_right(self):
+    def test_update__forward_right(self):
         self.odom.update_from_velocity(pi / 2, 0, 1)
         self.assert_odom_equal(0.5, -0.5, -pi / 2)
 
-    def test_update_backward_left(self):
+    def test_update__backward_left(self):
         self.odom.update_from_velocity(0, -pi / 2, 1)
         self.assert_odom_equal(-0.5, 0.5, -pi / 2)
 
-    def test_update_backward_right(self):
+    def test_update__backward_right(self):
         self.odom.update_from_velocity(-pi / 2, 0, 1)
         self.assert_odom_equal(-0.5, -0.5, pi / 2)
 
-    def test_update_detailed(self):
-        self.odom = DifferentialOdometry(1.23)
+    def test_update__detailed(self):
+        self.odom = odometry.DifferentialOdometry(1.23)
         self.odom.update_from_velocity(0.2, 0.23, 4.5)
         self.assert_odom_equal(0.9655587, 0.0530412, 0.1097561)
 
-    def test_update_multiple(self):
+    def test_update__multiple(self):
         self.odom.update_from_velocity(1, 1, 1)
         self.assert_odom_equal(1, 0, 0)
 
