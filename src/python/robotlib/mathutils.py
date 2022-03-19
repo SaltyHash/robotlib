261556c287c930a052b0c908733dc180b5117c76
Austin Bowen <austin.bowen.314@gmail.com>
2022-03-18 23:32:04 -0400

    Add mathutils module with Clipper and LinearExtrapolator

---
+++ src/python/robotlib/mathutils.py
@@ -0,0 +1,103 @@
+from typing import Optional
+
+
+class Clipper:
+    def __init__(
+            self,
+            min_value: Optional[float],
+            max_value: Optional[float],
+    ):
+        self._min_value = None
+        self._min_clipper = None
+
+        self._max_value = None
+        self._max_clipper = None
+
+        self.set_limits(min_value, max_value)
+
+    def set_limits(
+            self,
+            min_value: Optional[float],
+            max_value: Optional[float]
+    ) -> None:
+        # Clear the max in case it happens to already be set to a value that is
+        # lower than the new min_value, which would throw an exception
+        self.set_max(None)
+
+        self.set_min(min_value)
+        self.set_max(max_value)
+
+    def get_min(self) -> Optional[float]:
+        return self._min_value
+
+    def set_min(self, value: Optional[float]) -> None:
+        self._check_min(value)
+        self._min_value = value
+        self._set_min_clipper()
+
+    def _check_min(self, value: Optional[float]) -> None:
+        if value is None:
+            return
+
+        if self._max_value is not None and value > self._max_value:
+            raise ValueError(f'Min value ({value}) cannot be greater '
+                             f'than max value ({self._max_value}).')
+
+    def _set_min_clipper(self):
+        if self._min_value is not None:
+            self._min_clipper = lambda v: max(v, self._min_value)
+        else:
+            self._min_clipper = lambda v: v
+
+    def get_max(self) -> Optional[float]:
+        return self._max_value
+
+    def set_max(self, value: Optional[float]) -> None:
+        self._check_max(value)
+        self._max_value = value
+        self._set_max_clipper()
+
+    def _check_max(self, value: Optional[float]) -> None:
+        if value is None:
+            return
+
+        if self._min_value is not None and value < self._min_value:
+            raise ValueError(f'Max value ({value}) cannot be greater '
+                             f'than min value ({self._min_value}).')
+
+    def _set_max_clipper(self) -> None:
+        if self._max_value is not None:
+            self._max_clipper = lambda v: min(v, self._max_value)
+        else:
+            self._max_clipper = lambda v: v
+
+    def __call__(self, value: float) -> float:
+        return self.clip(value)
+
+    def clip(self, value: float) -> float:
+        value = self._min_clipper(value)
+        value = self._max_clipper(value)
+        return value
+
+
+class LinearExtrapolator:
+    def __init__(
+            self,
+            x0: float, y0: float, x1: float, y1: float,
+            min_output: float = None,
+            max_output: float = None
+    ):
+        self._x0 = x0
+        self._y0 = y0
+
+        self._m = (y1 - y0) / (x1 - x0)
+
+        self._clipper = Clipper(min_output, max_output)
+
+    def __call__(self, x: float) -> float:
+        return self.extrapolate(x)
+
+    def extrapolate(self, x) -> float:
+        value = self._m * (x - self._x0) + self._y0
+        value = self._clipper(value)
+        return value
---
+++ test/python/robotlib_tests/test_mathutils.py
@@ -0,0 +1,134 @@
+import unittest
+from typing import Optional
+from unittest import TestCase
+
+from parameterized import parameterized
+
+from robotlib.mathutils import Clipper, LinearExtrapolator
+
+
+class TestClipper(TestCase):
+    @parameterized.expand([
+        # [min_value, max_value, x, expected_y]
+        [-5, 10, 0, 0],
+        [-5, 10, -4, -4],
+        [-5, 10, -5, -5],
+        [-5, 10, -6, -5],
+        [None, 10, -6, -6],
+        [-5, 10, 9, 9],
+        [-5, 10, 10, 10],
+        [-5, 10, 11, 10],
+        [-5, None, 11, 11],
+    ])
+    def test_clip(
+            self,
+            min_value: Optional[float],
+            max_value: Optional[float],
+            x: float,
+            expected_y: float
+    ) -> None:
+        clipper = Clipper(min_value, max_value)
+
+        actual_y = clipper.clip(x)
+
+        self.assertAlmostEqual(expected_y, actual_y)
+
+    def test_call(self) -> None:
+        clipper = Clipper(-2, 5)
+        x = 10
+
+        clip_result = clipper.clip(x)
+        call_result = clipper(x)
+
+        self.assertEqual(clip_result, call_result)
+
+    def test_get_min(self) -> None:
+        clipper = Clipper(3, 10)
+
+        actual = clipper.get_min()
+
+        self.assertEqual(3, actual)
+
+    def test_set_min__good_value(self) -> None:
+        clipper = Clipper(None, 10)
+
+        clipper.set_min(5)
+        actual = clipper.get_min()
+
+        self.assertEqual(5, actual)
+
+    def test_set_min__bad_value__raise_ValueError(self) -> None:
+        clipper = Clipper(5, 10)
+
+        with self.assertRaises(ValueError) as assert_context:
+            clipper.set_min(20)
+
+        self.assertEqual(
+            'Min value (20) cannot be greater than max value (10).',
+            str(assert_context.exception)
+        )
+
+    def test_get_max(self) -> None:
+        clipper = Clipper(3, 10)
+
+        actual = clipper.get_max()
+
+        self.assertEqual(10, actual)
+
+    def test_set_max(self) -> None:
+        clipper = Clipper(5, None)
+
+        clipper.set_max(10)
+        actual = clipper.get_max()
+
+        self.assertEqual(10, actual)
+
+    def test_set_max__bad_value__raise_ValueError(self) -> None:
+        clipper = Clipper(5, 10)
+
+        with self.assertRaises(ValueError) as assert_context:
+            clipper.set_max(0)
+
+        self.assertEqual(
+            'Max value (0) cannot be greater than min value (5).',
+            str(assert_context.exception)
+        )
+
+
+class TestLinearExtrapolator(TestCase):
+    @parameterized.expand([
+        # [x, expected_y]
+        [0, 0],
+        # - Positive x
+        [1.0, -2],
+        [5.0, -10],
+        [9.5, -19],
+        [10.0, -20],
+        [10.5, -20],
+        # - Negative x
+        [-1, 2],
+        [-4.5, 9],
+        [-5.0, 10],
+        [-5.5, 10]
+    ])
+    def test_extrapolate(
+            self,
+            x: float,
+            expected_y: float
+    ) -> None:
+        extrapolator = LinearExtrapolator(
+            x0=1,
+            y0=-2,
+            x1=5,
+            y1=-10,
+            min_output=-20,
+            max_output=10
+        )
+
+        actual_y = extrapolator.extrapolate(x)
+
+        self.assertAlmostEqual(expected_y, actual_y)
+
+
+if __name__ == '__main__':
+    unittest.main()
