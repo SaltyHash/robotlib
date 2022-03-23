7d35220599b54d212a92548efea240746161a548
Austin Bowen <austin.bowen.314@gmail.com>
2022-03-22 23:22:27 -0400

    Refactor LowPassFilter to be subclass of _SimpleFilter

--- src/python/robotlib/signals/filters.py
+++ src/python/robotlib/signals/filters.py
@@ -1,11 +1,7 @@
 from math import pi
 
 
-class LowPassFilter:
-    """
-    https://en.wikipedia.org/wiki/Low-pass_filter
-    """
-
+class _SimpleFilter:
     def __init__(self, cutoff_freq: float, init_value: float = 0.0):
         self._cutoff_freq = None
         self.set_cutoff_freq(cutoff_freq)
@@ -16,15 +12,42 @@ class LowPassFilter:
         return self._cutoff_freq
 
     def set_cutoff_freq(self, cutoff_freq: float) -> None:
+        self._check_cutoff_freq(cutoff_freq)
+        self._cutoff_freq = cutoff_freq
+
+    def _check_cutoff_freq(self, cutoff_freq: float) -> None:
         if cutoff_freq < 0:
             raise ValueError(f'cutoff_freq must be >= 0; got {cutoff_freq}.')
-
-        self._cutoff_freq = cutoff_freq
 
     def filter(self, value: float, dt: float) -> float:
         output = self._filter(value, dt)
         self._prev_output = output
         return output
+
+    def _filter(self, value: float, dt: float) -> float:
+        alpha = self._get_alpha(dt)
+        return alpha * value + (1 - alpha) * self._prev_output
+
+    def _get_alpha(self, dt: float) -> float:
+        cutoff_freq = self.get_cutoff_freq()
+        a = 2 * pi * dt * cutoff_freq
+        return a / (a + 1)
+
+
+class LowPassFilter(_SimpleFilter):
+    """
+    Passes signals lower than the cutoff frequency. Decreases signals higher
+    than the cutoff frequency. The higher a signal's frequency is above the
+    cutoff frequency, the more the signal is decreased.
+
+    This is also known as a "moving average".
+
+    This is useful for example if you have a sensor reading that is really
+    "noisy" (perhaps an accelerometer or distance sensor), and you want to get a
+    sort of averaged, slower-moving reading from the noisy readings.
+
+    https://en.wikipedia.org/wiki/Low-pass_filter
+    """
 
     def _filter(self, value: float, dt: float) -> float:
         alpha = self._get_alpha(dt)
