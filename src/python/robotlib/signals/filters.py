e43afa644d30a7614e131f3d41cfb70921e4c53d
Austin Bowen <austin.bowen.314@gmail.com>
2022-03-22 23:39:47 -0400

    Add HighPassFilter

--- src/python/robotlib/signals/filters.py
+++ src/python/robotlib/signals/filters.py
@@ -25,13 +25,7 @@ class _SimpleFilter:
         return output
 
     def _filter(self, value: float, dt: float) -> float:
-        alpha = self._get_alpha(dt)
-        return alpha * value + (1 - alpha) * self._prev_output
-
-    def _get_alpha(self, dt: float) -> float:
-        cutoff_freq = self.get_cutoff_freq()
-        a = 2 * pi * dt * cutoff_freq
-        return a / (a + 1)
+        raise NotImplementedError()
 
 
 class LowPassFilter(_SimpleFilter):
@@ -57,3 +51,32 @@ class LowPassFilter(_SimpleFilter):
         cutoff_freq = self.get_cutoff_freq()
         a = 2 * pi * dt * cutoff_freq
         return a / (a + 1)
+
+
+class HighPassFilter(_SimpleFilter):
+    """
+    Passes signals higher than the cutoff frequency. Decreases signals lower
+    than the cutoff frequency. The lower a signal's frequency is below the
+    cutoff frequency, the more the signal is decreased.
+
+    This is useful for example if you have a sensor reading that has a slowly
+    changing bias, for example a gyroscope. This can remove the bias from the
+    rapidly varying true signal.
+
+    https://en.wikipedia.org/wiki/High-pass_filter
+    """
+
+    def __init__(self, cutoff_freq: float, init_value: float = 0.0):
+        super().__init__(cutoff_freq, init_value)
+        self._prev_value = init_value
+
+    def _filter(self, value: float, dt: float) -> float:
+        d_value = value - self._prev_value
+        self._prev_value = value
+
+        alpha = self._get_alpha(dt)
+        return alpha * (self._prev_output + d_value)
+
+    def _get_alpha(self, dt: float) -> float:
+        cutoff_freq = self.get_cutoff_freq()
+        return 1 / (2 * pi * dt * cutoff_freq + 1)
--- test/python/robotlib_tests/signals/test_filters.py
+++ test/python/robotlib_tests/signals/test_filters.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from math import pi
 
-from robotlib.signals.filters import LowPassFilter
+from robotlib.signals.filters import LowPassFilter, HighPassFilter
 
 
 class TestLowPassFilter(TestCase):
@@ -40,6 +40,9 @@ class TestLowPassFilter(TestCase):
         output = f.filter(1, dt)
         self.assertAlmostEqual(0.75, output)
 
+        output = f.filter(1, dt)
+        self.assertAlmostEqual(0.875, output)
+
     def test_filter__complex(self):
         f = LowPassFilter(120)
         dt = 1 / 44100
@@ -49,3 +52,53 @@ class TestLowPassFilter(TestCase):
 
         output = f.filter(0.34, dt)
         self.assertAlmostEqual(0.0095165, output)
+
+
+class TestHighPassFilter(TestCase):
+    def test_set_cutoff_freq__0__good(self):
+        f = HighPassFilter(1)
+        f.set_cutoff_freq(0)
+
+    def test_set_cutoff_freq__10__good(self):
+        f = HighPassFilter(1)
+        f.set_cutoff_freq(10)
+
+    def test_set_cutoff_freq__less_than_0__raises_ValueError(self):
+        f = HighPassFilter(1)
+
+        with self.assertRaises(ValueError):
+            f.set_cutoff_freq(-0.01)
+
+    def test_filter__cutoff_freq_0(self):
+        f = HighPassFilter(0)
+
+        output = f.filter(1, 0.1)
+        self.assertAlmostEqual(1.0, output)
+
+        output = f.filter(0.5, 0.1)
+        self.assertAlmostEqual(0.5, output)
+
+    def test_filter__simple(self):
+        f = HighPassFilter(1)
+
+        # This special dt will make alpha = 0.5
+        dt = 1 / (2 * pi)
+
+        output = f.filter(1, dt)
+        self.assertAlmostEqual(0.5 / 1, output)
+
+        output = f.filter(1, dt)
+        self.assertAlmostEqual(0.25, output)
+
+        output = f.filter(1, dt)
+        self.assertAlmostEqual(0.125, output)
+
+    def test_filter__complex(self):
+        f = HighPassFilter(120)
+        dt = 1 / 44100
+
+        output = f.filter(0.23, dt)
+        self.assertAlmostEqual(0.2261338, output)
+
+        output = f.filter(0.34, dt)
+        self.assertAlmostEqual(0.3304835, output)
